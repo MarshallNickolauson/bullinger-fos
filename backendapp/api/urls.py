b7c71be564d1526976f89e9a5187fd380df35c21
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-06-17 12:23:38 -0400

    refactored

--- backendapp/api/urls.py
+++ backendapp/api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('definitions', views.DefinitionView.as_view({'get':'list', 'post':'create'})),
-    path('definitions/<int:pk>', views.DefinitionView.as_view({'get':'retrieve', 'put':'update', 'patch':'partial_update', 'delete':'destroy'})),
+    path('definitions/<int:pk>', views.DefinitionView.as_view({'get':'retrieve', 'put':'update', 'delete':'destroy'})),
     path('usages', views.UsageView.as_view({'get':'list', 'post':'create'})),
     path('usages/<int:pk>', views.UsageView.as_view({'get':'retrieve', 'put':'update', 'patch':'partial_update', 'delete':'destroy'})),
 ]
--- backendapp/api/views.py
+++ backendapp/api/views.py
@@ -9,6 +9,14 @@ class DefinitionView(ModelViewSet):
     queryset = Definition.objects.all()
     serializer_class = DefinitionSerializer
     
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+    
 class UsageView(ModelViewSet):
     queryset = Usage.objects.all()
     serializer_class = UsageSerializer
