a018964b3903497bbf6b011d63bf73491570b133
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-06-18 19:10:34 -0400

    started bootstrap layout

--- frontendapp/src/App.js
+++ frontendapp/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import FiguresDev from './components/dev/FiguresDev';
 import FigureDev from './components/dev/FigureDev';
-
+import HomePage from './components/main/HomePage';
+import TopNavbar from './components/main/TopNavBar';
 
 function App() {
   return (
     <Router>
-      <div className='container-xl'>
+      <div>
+        <TopNavbar />
         <Routes>
+          <Route path='/' element={<HomePage />} />
           <Route path='/dev/figures' element={<FiguresDev />} />
           <Route path='/dev/figures/:id' element={<FigureDev />} />
         </Routes>
---
+++ frontendapp/src/components/main/TopNavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function TopNavbar() {
+
+    return (
+        <nav className="navbar navbar-expand-fluid navbar-light bg-light">
+            <div className="container-xxl">
+                <a className="navbar-brand" href="#"><strong>Bullinger FoS</strong></a>
+            </div>
+
+        </nav>
+    )
+}
