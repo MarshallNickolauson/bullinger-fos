c2fafda57301843a44eacbe1ea38c8c70e31fed3
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-06-19 12:50:41 -0400

    added edit button for modal

--- frontendapp/src/components/main/DefinitionDetail.js
+++ frontendapp/src/components/main/DefinitionDetail.js
@@ -6,6 +6,9 @@ export default function DefinitionDetail({ record, isDefinitionExpanded, toggleD
     const figure_name = record['figure_name'];
     const definition = record['content'];
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [editedContent, setEditedContent] = useState(definition);
+
     const navigate = useNavigate();
 
     const handlePrevClick = () => {
@@ -32,9 +35,23 @@ export default function DefinitionDetail({ record, isDefinitionExpanded, toggleD
         return sentences.slice(0, 2).join('. ') + '...';
     };
 
+    const formatContent = (content) => {
+        return content.split('\n').map((line, index) => (
+            <React.Fragment key={index}>
+                {line}
+                <br />
+            </React.Fragment>
+        ));
+    };
+
     const toggleExpand = () => {
         toggleDefinitionExpand();
     };
+
+    const handleEditClick = (event) => {
+        event.stopPropagation();
+        console.log('modal state');
+    }
 
     return (
         <>
@@ -59,14 +76,17 @@ export default function DefinitionDetail({ record, isDefinitionExpanded, toggleD
                             className="btn btn-outline-dark"
                             disabled={id === 201}
                             onClick={handleNextClick}>
-                            Prev
+                            Next
                         </button>
                     </div>
                 </div>
 
-                <div className="definition-card" onClick={toggleExpand}>
+                <div className="definition-card container" onClick={toggleExpand}>
                     <div className={`content ${isDefinitionExpanded ? 'expanded' : 'collapsed'}`}>
-                        {isDefinitionExpanded ? definition : getPreviewText(definition)}
+                        {isDefinitionExpanded ? formatContent(definition) : getPreviewText(definition)}
+                        {isDefinitionExpanded && (
+                            <button type='button' className='btn btn-outline-dark edit-button' onClick={handleEditClick}>Edit</button>
+                        )}
                     </div>
                     <div className="toggle-indicator">
                         {isDefinitionExpanded ? '...' : '...'}
--- frontendapp/src/components/main/Figure.js
+++ frontendapp/src/components/main/Figure.js
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 import config from '../../config/config';
 import DefinitionDetail from './DefinitionDetail';
 
 export default function Figure({ definitions, usages }) {
     const { id } = useParams();
+    const location = useLocation();
 
     const stringId = String(id);
     const definitionData = definitions.find(def => String(def.id) === stringId);
     const usageData = usages.find(usage => String(usage.id) === stringId);
 
     const [isDefinitionExpanded, setIsDefinitionExpanded] = useState(false);
+
+    useEffect(() => {
+        setIsDefinitionExpanded(false);
+    }, [location]);
 
     const toggleDefinitionExpand = () => {
         setIsDefinitionExpanded(!isDefinitionExpanded);
