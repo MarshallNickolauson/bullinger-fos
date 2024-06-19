9edffcafd9c721116cbfc04533e018fb6bcda1ba
marsh <marshnickol101>
2024-06-18 20:16:17 -0400

    fetching definition data for sidebar nav

--- frontendapp/src/App.js
+++ frontendapp/src/App.js
@@ -6,8 +6,35 @@ import HomePage from './components/main/HomePage';
 import TopNavbar from './components/main/TopNavBar';
 import SideNavbar from './components/main/SideNavBar';
 import AboutPage from './components/main/AboutPage';
+import { useEffect, useState } from 'react';
+import config from './config/config';
 
 function App() {
+
+  const [definitionData, setDefinitionData] = useState(null);
+
+  useEffect(() => {
+    const definitionApiEndpoint = `${config.apiBaseUrl}/definitions`;
+
+    fetch(definitionApiEndpoint, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }).then(response => {
+      if (!response.ok) {
+        return response.json().then(error => {
+          throw new Error(`Failed to get content: ${JSON.stringify(error)}`)
+        });
+      }
+      return response.json();
+    }).then(data => {
+      setDefinitionData(data);
+    }).catch(e => {
+      console.error('Error getting content: ', e);
+    });
+  }, []);
+
   return (
     <Router>
       <div>
@@ -20,6 +47,8 @@ function App() {
             <Routes>
               <Route path='/' element={<HomePage />} />
               <Route path='/about' element={<AboutPage />} />
+
+
               <Route path='/dev/figures' element={<FiguresDev />} />
               <Route path='/dev/figures/:id' element={<FigureDev />} />
             </Routes>
