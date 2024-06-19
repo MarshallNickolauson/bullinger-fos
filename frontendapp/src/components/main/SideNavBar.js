3ae506b337c4ef0b2c2b4db5c2479ad386e0fe2c
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-06-19 17:03:53 -0400

    added button to sort side navbar by a-z or book appearance

--- frontendapp/src/components/main/SideNavBar.js
+++ frontendapp/src/components/main/SideNavBar.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function SideNavbar({ data }) {
+
+  const [bookSort, setBookSort] = useState(true);
 
   const sortedDefinitionData = [...data].sort((a, b) => {
     if (a.figure_name < b.figure_name) {
@@ -13,6 +15,10 @@ export default function SideNavbar({ data }) {
     return 0;
   });
 
+  const handleSortClick = () => {
+    setBookSort(!bookSort);
+  };
+
   const capitalizeFirstLetter = (string) => {
     if (!string) return '';
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -21,10 +27,23 @@ export default function SideNavbar({ data }) {
   return (
     <div className="fixed-side pt-5 mt-3 side-navbar">
       <nav className="nav flex-column">
+        <button type="button" className="btn btn-outline-dark mx-1 mb-1" onClick={handleSortClick}>
+          {bookSort ? 'Alphabetical' : 'Book Appearance'}
+        </button>
         <Link to='/introduction' className="book-section-link">Introduction</Link>
-        {sortedDefinitionData.map((item, index) => (
-          <Link to={`/figures/${item.id}`} className="book-section-link" key={index}>{capitalizeFirstLetter(item.figure_name)}</Link>
-        ))}
+        {bookSort ? (
+          <>
+            {sortedDefinitionData.map((item, index) => (
+              <Link to={`/figures/${item.id}`} className="book-section-link" key={index}>{capitalizeFirstLetter(item.figure_name)}</Link>
+            ))}
+          </>
+        ) : (
+          <>
+            {data.map((item, index) => (
+              <Link to={`/figures/${item.id}`} className="book-section-link" key={index}>{capitalizeFirstLetter(item.figure_name)}</Link>
+            ))}
+          </>
+        )}
       </nav>
     </div>
   );
