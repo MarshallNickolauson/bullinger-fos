5a37a8481e1171f2d0c1aa24adecb7ce3c59fa40
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-06-21 06:39:30 -0400

    began rebuilding top navbar

--- frontendapp/src/components/main/TopNavBar.js
+++ frontendapp/src/components/main/TopNavBar.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import '../../css/TopNavBar.css'
 
 export default function TopNavbar() {
+    const [isNavVisible, setIsNavVisible] = useState(false);
+
+    const toggleNav = () => {
+      setIsNavVisible(!isNavVisible);
+    }
 
     return (
         <div className="fixed-top top-navbar" >
             <nav className="navbar navbar-expand-lg bg-white">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#"><strong>Bullinger FoS</strong></a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="nav-toggle" type="button" onClick={toggleNav}>
                         &#9776;
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
