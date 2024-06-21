df89eafc19e56fee7d335c707594e9c300286309
marsh <marshnickol101>
2024-06-21 08:36:29 -0400

    rebuilt navbar

--- frontendapp/src/components/main/TopNavBar.js
+++ frontendapp/src/components/main/TopNavBar.js
@@ -10,10 +10,28 @@ export default function TopNavbar() {
     }
 
     return (
-        <div className="fixed-top top-navbar" >
-            <nav>
-                
-            </nav>
-        </div>
+        <header className="fixed-top app-header">
+            <div className="container-fluid">
+                <div className="row align-items-center">
+                    <div className="col-2">
+                        <Link className="app-logo" to='/'>Bullinger FoS</Link>
+                    </div>
+                    <div className="col-10 nav-space">
+                        <nav className={`nav ${isNavVisible ? 'nav-visible' : ''}`}>
+                            <ul className="nav-list">
+                                <li className="nav-item"><Link to="/">Home</Link></li>
+                                <li className="nav-item"><Link to="/about">About</Link></li>
+                            </ul>
+                            <div className="search-bar">
+                                <input type="text" className="search-input" placeholder="Search..." />
+                                <button className="search-button">Search</button>
+                            </div>                        </nav>
+                        <button className="nav-toggle" onClick={toggleNav}>
+                            &#9776;
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </header>
     )
 }
