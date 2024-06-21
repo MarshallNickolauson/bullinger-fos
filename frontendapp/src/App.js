62585e728659b62c4348ab0ec0394699b8e4b392
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-06-21 16:51:09 -0400

    removed dev components

--- frontendapp/src/App.js
+++ frontendapp/src/App.js
@@ -1,7 +1,5 @@
 import '../src/css/App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import FiguresDev from './components/dev/FiguresDev';
-import FigureDev from './components/dev/FigureDev';
 import HomePage from './components/main/HomePage';
 import TopNavbar from './components/main/TopNavBar';
 import SideNavbar from './components/main/SideNavBar';
@@ -85,9 +83,6 @@ function App() {
               <Route path='/' element={<HomePage />} />
               <Route path='/about' element={<AboutPage />} />
               <Route path='/figures/:id' element={<Figure definitions={definitionData} usages={usageData} onUpdateDefinition={handleUpdateDefinition} onUpdateUsage={handleUpdateUsage} />} />
-
-              <Route path='/dev/figures' element={<FiguresDev />} />
-              <Route path='/dev/figures/:id' element={<FigureDev />} />
             </Routes>
           </main>
         </div>
