0cad5f22b1ba64302df0d30abd7c48346ab69034
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-08-06 20:25:38 -0400

    introduction extracted

--- extraction/scripts/full_doc_extraction.py
+++ extraction/scripts/full_doc_extraction.py
@@ -2,18 +2,16 @@ from pathlib import Path
 from PIL import Image
 import pytesseract
 
-start_page = 41
-end_page = 2375
-image_dir = r'C:\Users\Marshall\Desktop\Coding\Python\bullinger-fos-app\pages'
-output_file_path = Path(r'C:\Users\Marshall\Desktop\Coding\Python\bullinger-fos-app\full_document.txt')
+image_dir = r'C:\Users\Marshall\Desktop\Coding\Python\bullinger-fos-app\extraction\pages'
+output_file_path = Path(r'C:\Users\Marshall\Desktop\Coding\Python\bullinger-fos-app\introduction_extraction.txt')
 custom_config = r'--oem 3 --psm 6'
 
 with open(output_file_path, 'w', encoding='utf-8') as text_file:
-    for page_num in range(start_page, end_page + 1):
+    for page_num in range(1, 33):
         image_path = Path(image_dir) / f'page_{page_num}.png'
         image = Image.open(image_path)
         text = pytesseract.image_to_string(image, lang='eng+ell+rus', config=custom_config)
         text_file.write(text)
         print(f"Extracted page {page_num}")
 
-print(f"Extracted text from pages {start_page} to {end_page} has been saved to {output_file_path}")
+print("done")
